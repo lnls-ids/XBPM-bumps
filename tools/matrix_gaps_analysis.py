a0dd3c78ccff8b3316cd636d7c673b87c4f5a727
Caio Marcilio dos Santos <caio.santos@lnls.br>
2026-05-19 14:41:30 -0300

    feat: Improved usability and data handling
    - The script can now run with arguments to handle the HDF5 files path and XBPM name and number
    
    - Improved graphs display by changing the colors, organizing the plots and renamed some variables and titles
    
    - First data validation functions deployed, to check if the given path exists and if it has HDF5 files.

--- tools/matrix_gaps_analysis.py
+++ tools/matrix_gaps_analysis.py
@@ -5,9 +5,6 @@
 This script uses the suppression matrixes obtained by the XBPM analisys
 to calculate and check if there is a significant relationship between matrixes
 given they were acquired with different gaps for the same beamline undulator.
-
->>> Run this script under "sirius' environment, to use
->>> pymodels and pyaccel libraries.
 """
 
 import glob
@@ -16,55 +13,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
-parser = argparse.ArgumentParser(description="Analyze relationships between XBPM suppression matrixes.")
-parser.add_argument("--xbpm", help="XBPM name (e.g., MNC1, MNC2)")
-parser.add_argument("--file_path", help="Path to XBPM data files")
-args = parser.parse_args()
+desc = """This script uses the suppression matrixes obtained by the XBPM analisys
+to calculate and check if there is a significant relationship between matrixes
+given they were acquired with different gaps for the same beamline undulator."""
+coefs = ['B', 'C', 'D']
 
-xbpm_data = {}
-phase = [5.419, 2.818, 0.148, 4.536]
+def _check_file_path(path):
+    if not glob.glob(f"{path}"):
+        raise FileNotFoundError(f"The specified path does not exist: {path}")
+    if not glob.glob(f"{path}/*.h5") and not glob.glob(f"{path}/*.hdf5"):
+        raise FileNotFoundError(f"No HDF5 files found in the specified path: {path}")
 
-# Read HDF5 files containing the suppression matrixes
-for file in glob.glob("/home/caio.santos/Documents/XBPM_analysis/MNC1/MNC1_*.h5"):
-    print(f"Found file: {file}")
+
+def _check_hdf5_structure(file):
+    required_groups = [
+        f"/analysis_{args.xbpm}/matrices/calculated", 
+        f"/analysis_{args.xbpm}/matrices/stddev", 
+        "/raw_data/sweep_0000"
+        ]
     with h5py.File(file, "r") as f:
-        idx = int(file.split("_")[-1].split(".")[0])
-        xbpm_data[idx] = {}
-        xbpm_data[idx]['supmat'] = f["/analysis_MNC1/matrices/calculated"][:]
-        xbpm_data[idx]['stddev'] = f["/analysis_MNC1/matrices/stddev"][:]
-        xbpm_data[idx]['phase'] = phase[int(file.split("_")[-1].split(".")[0]) - 1]
+        for group in required_groups:
+            if group not in f:
+                raise KeyError(f"Required group '{group}' not found in file: {file}")
+            if group == "/raw_data/sweep_0000":
+                if f[group].attrs.get(f'{args.xbpm[:3].lower()} gap') is None:
+                    raise KeyError(f"Required attribute '{args.xbpm[:3].lower()} gap' not"
+                                   "found in group '/raw_data/sweep_0000' of file: {file}")
 
-print(f"MNC1 data: {xbpm_data}")
 
 def _get_matrixes_coefs(data, row, col):
-    idx = []
-    for i in range(len(data)):
-        matrix = data[i+1]['supmat']
+    idx = list()
+    for i in data:
+        matrix = abs(data[i]['supmat'])
         idx.append(matrix[row, col])
     return idx
 
 
 def _get_matrixes_errors(data, row, col):
-    errors = []
-    for i in range(len(data)):
-        stddev = data[i+1]['stddev']
+    errors = list()
+    for i in data:
+        stddev = data[i]['stddev']
         errors.append(stddev[row, col])
     return errors
 
 
 def _fit_coef_curve(phase, coefs, degree):
-    fit = np.polyfit(sorted(phase), coefs, degree)
+    fit = np.polyfit(phase, coefs, degree)
     print(f"{degree}-degree fit coefficients: {fit}")
-    return np.polyval(fit, sorted(phase))
+    return np.polyval(fit, phase)
 
 
 def _plot_coefs_results(ax, phase, coefs, fitted_1vals, fitted_2vals, errors, title):
-    ax.plot(sorted(phase), fitted_1vals, label='Linear Fit', linestyle='--', marker='x')
-    ax.plot(sorted(phase), fitted_2vals, label='Quadratic Fit', linestyle='--', marker='x')
-    ax.plot(sorted(phase), coefs, label='Matrixes Coefs.', marker='o')
-    ax.errorbar(sorted(phase), coefs, yerr=errors, fmt='o', color='green', capsize=2, label='Propagated Error')
-    ax.set_xticks(sorted(phase))
-    ax.set_xlabel('Phase (mm)')
+    ax.plot(phase, fitted_1vals, label='Linear Fit', color='red', linestyle='--', marker='x')
+    ax.plot(phase, fitted_2vals, label='Quadratic Fit', color='orange', linestyle='--', marker='s')
+    ax.errorbar(phase, coefs, yerr=errors, fmt='o-', color='tab:blue', capsize=2, label='Matrix Coeffs.')
+    ax.set_xticks(phase)
+    ax.set_xlabel('Phase/Gap (mm)')
     ax.set_ylabel('Coefficient value (a.u.)')
     ax.set_title(title)
     ax.grid()
@@ -72,9 +76,31 @@ def _plot_coefs_results(ax, phase, coefs, fitted_1vals, fitted_2vals, errors, ti
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="matrix_gaps_analysis", description=desc)
+    parser.add_argument("-x", "--xbpm", required=True, help="XBPM name (e.g., MNC1, MNC2)")
+    parser.add_argument("-p", "--path", required=True, help="Path to XBPM HDF5 processed data files")
+    args = parser.parse_args()
+
+    # Read HDF5 files containing the suppression matrixes
+    xbpm_data = dict()
+    _check_file_path(f"{args.path}")
+    for file in glob.glob(f"{args.path}/*.h5"):
+        print(f"Found file: {file}")
+        _check_hdf5_structure(file)
+        with h5py.File(file, "r") as f:
+            idx = int(file.split("_")[-1].split(".")[0])
+            xbpm_data[idx] = {}
+            xbpm_data[idx]['supmat'] = f[f"/analysis_{args.xbpm}/matrices/calculated"][:]
+            xbpm_data[idx]['stddev'] = f[f"/analysis_{args.xbpm}/matrices/stddev"][:]
+            gap = f["/raw_data/sweep_0000"].attrs[f'{args.xbpm[:3].lower()} gap']
+            xbpm_data[idx]['gap'] = round(gap, 4)
+
+    xbpm_data = dict(sorted(xbpm_data.items(), key=lambda item: item[1]['gap']))
+    print(f"{args.xbpm} data: {xbpm_data}")
+
     # Plot coeffitiens vs phase with linear fit
-    fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(16, 9))
-    fig.suptitle("XBPM Suppression Matrixes Coefficients and Undulator Phase Correlations - MNC1", fontsize=16)
+    fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(15, 10))
+    fig.suptitle(f"{args.xbpm} XBPM Suppression Matrices Coeffs. & Undulator Phase/Gap Relationship", fontsize=16)
     for i in range(0, 4, 2):
         for j in range(1, 4):
             print(f"\nCoefficient a[{i}{j}]")
@@ -82,12 +108,14 @@ if __name__ == "__main__":
             print(f"Coefficients for each phase: {coef}")
             errors = _get_matrixes_errors(xbpm_data, i, j)
             print(f"Standard deviations for each phase: {errors}")
-            fitted_curve_1 = _fit_coef_curve(sorted(phase), coef, degree=1)
-            fitted_curve_2 = _fit_coef_curve(sorted(phase), coef, degree=2)
+            fitted_curve_1 = _fit_coef_curve([xbpm_data[idx]['gap'] for idx in xbpm_data], coef, degree=1)
+            fitted_curve_2 = _fit_coef_curve([xbpm_data[idx]['gap'] for idx in xbpm_data], coef, degree=2)
             if (i//2) < 1:
-                _plot_coefs_results(ax[i//2, j-1], phase, coef, fitted_curve_1, fitted_curve_2, errors, f'{chr(916)}x Coef a[{i}{j}] vs Phase')
+                _plot_coefs_results(ax[i//2, j-1], [xbpm_data[idx]['gap'] for idx in xbpm_data], coef, 
+                                    fitted_curve_1, fitted_curve_2, errors, f'{chr(916)}x {coefs[j-1]} vs Phase/Gap')
             else:
-                _plot_coefs_results(ax[i//2, j-1], phase, coef, fitted_curve_1, fitted_curve_2, errors, f'{chr(916)}y Coef a[{i}{j}] vs Phase')
+                _plot_coefs_results(ax[i//2, j-1], [xbpm_data[idx]['gap'] for idx in xbpm_data], coef, 
+                                    fitted_curve_1, fitted_curve_2, errors, f'{chr(916)}y {coefs[j-1]} vs Phase/Gap')
 
     plt.tight_layout()
     plt.show()
